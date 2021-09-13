6fe9c22c1c19a166a025aaf8b8931b5599ac9209
Aleksey <afed2000@yandex.ru>
2021-09-13 23:28:35 +0300

    App layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 const MainComponent = {
-    background: "lightyellow"
+    background: "lightyellow",
+    height: "32px",
+    padding: "10px",
+    fontSize: "24px",
+    textAlign: "center"
 }
 
 class Footer extends React.Component {
     render() {
       return (
           <div style={MainComponent}>
+              Осталось свободного места в пылесборнике: 235/500 мл.
           </div>
       );
     }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,34 @@
 import React from "react";
 
 const MainComponent = {
-    background: "lightblue"
+    background: "lightgreen",
+    height: "32px",
+    padding: "10px"
+}
+
+const LeftHeader = {
+    float: "left",
+    fontSize: "24px"
+}
+
+const RightHeader = {
+    float: "right",
+    fontSize: "24px"
 }
 
 class Header extends React.Component {
     render() {
-      return (
-          <div style={MainComponent}>
-          </div>
-      );
-    }
+        return (
+            <div style={MainComponent}>
+                <div style={LeftHeader}>
+                    Робот-пылесос
+                </div>
+                <div style={RightHeader}>
+                    Заряд батареи
+                </div>
+            </div>
+        );
+      }
 }
 
 export default Header
